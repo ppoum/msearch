2245dcd1daba29c58d70d02636be8f6fd0d5710f
ppoum <pam@poum.ca>
2022-12-26 18:29:26 -0500

    MC-2 Save server state to file when shutting down

--- dispatcher/src/main.rs
+++ dispatcher/src/main.rs
@@ -4,7 +4,9 @@ mod routes;
 mod ip_chunk_iterator;
 
 use std::collections::VecDeque;
+use std::fs;
 use std::net::Ipv4Addr;
+use std::path::Path;
 use std::sync::Mutex;
 use rocket::serde::{Deserialize, Serialize};
 use routes::scout_routes;
@@ -19,14 +21,25 @@ pub struct ServerState {
 
 #[rocket::main]
 async fn main() {
+    println!("Trying to load previously saved server state");
+    let server_state: ServerState;
+    if Path::new("./.dispatcher").exists() {
+        println!("Found file, loading it");
+        let data = fs::read_to_string("./.dispatcher").expect("Unable to read file");
+        server_state = rocket::serde::json::from_str(&data).expect("Invalid file contents");
+    } else {
+        println!("No saved state file found.");
+        server_state = ServerState {
+            ip_range: Mutex::new(IpChunkIterator::new()),
+            valid_ips: Mutex::new(VecDeque::new())
+        };
+    }
+
     let result = rocket::build()
         .mount("/scout", scout_routes::get_all_routes())
         .mount("/info", info_routes::get_all_routes())
         .mount("/client", client_routes::get_all_routes())
-        .manage(ServerState {
-            ip_range: Mutex::new(IpChunkIterator::new()),
-            valid_ips: Mutex::new(VecDeque::new())
-        })
+        .manage(server_state)
         .launch()
         .await.expect("Server failed unexpectedly");
 
@@ -34,5 +47,6 @@ async fn main() {
     let server_state = result.state::<ServerState>().unwrap();
     let json_state = rocket::serde::json::to_string(server_state)
         .expect("Error serializing server state");
-    println!("{}", json_state)
+    fs::write("./.dispatcher", json_state).expect("Unable to save state to disk");
+    println!("Success! Shutting down...")
 }
