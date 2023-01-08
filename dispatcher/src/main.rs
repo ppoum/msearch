f45ece2e0320a99678f20bc1e4472d4c08f2c32d
ppoum <pam@poum.ca>
2023-01-08 14:55:40 -0500

    MC-21 Restored server state serialization

--- dispatcher/src/main.rs
+++ dispatcher/src/main.rs
@@ -41,12 +41,29 @@ async fn main() -> io::Result<()> {
     };
 
     env_logger::init();
+    let state_copy = server_state.clone();
     HttpServer::new(move || {
         App::new()
             .wrap(Logger::default())
-            .app_data(server_state.clone())
+            .app_data(state_copy.clone())
             .service(get_client_scope())
             .service(get_scout_scope())
             .service(get_info_scope())
-    }).bind(("0.0.0.0", 8000))?.run().await
+    }).bind(("0.0.0.0", 8000))?.run().await.expect("HttpServer panicked!");
+
+    // Save to server state to disk
+    println!("Saving current state to disk.");
+    // Start by adding outstanding jobs back to valid_ips pool
+    {
+        let mut outstanding = server_state.outstanding_client_jobs.lock().unwrap();
+        let mut valid_ips = server_state.valid_ips.lock().unwrap();
+        outstanding.iter().for_each(|x| valid_ips.push_front(x.ip));
+        outstanding.clear();
+    }
+    // Serialize to json, save to disk
+    let json_val = serde_json::to_string(&server_state.into_inner())
+        .expect("Error serializing server state");
+    fs::write("./.dispatcher", json_val).expect("Unable to save state to disk.");
+    println!("Success! Shutting down...");
+    Ok(())
 }
